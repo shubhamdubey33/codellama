b13886c8b5e9a38a6a0b1d4fff8146eca785045c
Shubham Dubey <67332387+shubhamdubey33@users.noreply.github.com>
2024-05-18 15:30:29 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,10 +3,10 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-read -p "Enter the URL from email: " PRESIGNED_URL
+read -p "https://download5.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoiNHozZXdnbDlzaGZjZWxsa3RpcW93bHQ3IiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQ1LmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MTYxMTE4NTR9fX1dfQ__&Signature=b7PSOl0KhXzqtqn3fXpZF0siTvQ0MSVUXfSA27JT8H4GYncmrqU4Na2Igp5PcNQt0R6kP63fzIij2E5EDhUhO54nKusRK3Ts3qC%7EUieNpq239oDve4QUsa8ZwIzQhVY36fT21P7ifQ1r4VCuCGQbXgDcFC2FhaJemNU8DqLO8a1muBjYmbFAYr%7ElwoFA-ft93mPowe8vCN45lvrKIrZiuthj-SToGp5xLJ5p6WifS50P3gQu-vldoi58ZPg-Lqr9jElEumtr6FEcrfzSE%7Esg6cWR9MeylKme7GstN40HrWNn%7E%7ESG--eGmirVVt%7ERkOqwxZ7kaJBDYo6tYC0uvfgvFg__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=760925566229198" PRESIGNED_URL
 echo ""
 ALL_MODELS="7b,13b,34b,70b,7b-Python,13b-Python,34b-Python,70b-Python,7b-Instruct,13b-Instruct,34b-Instruct,70b-Instruct"
-read -p "Enter the list of models to download without spaces ($ALL_MODELS), or press Enter for all: " MODEL_SIZE
+read -p "7b" MODEL_SIZE
 TARGET_FOLDER="."             # where all files should end up
 mkdir -p ${TARGET_FOLDER}
 
